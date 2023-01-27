ed38d97c4f44393a18b8c0de07395ee01221bd32
Qazi Ahmad <107860183+QaziAhmad77@users.noreply.github.com>
2023-01-27 21:44:55 +0500

    updatedg

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
-import Todo from "./components/todoreact/todo";
 // import Temp from "./components/weather/temp";
+import Todo from "./components/todoreact/todo";
 
 const App = () => {
   return (
     <>
+      {/* <Temp /> */}
       <Todo />
     </>
   );
