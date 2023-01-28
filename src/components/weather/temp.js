e5260009a0ea64bfa047fd5e28af3dc37ed43c98
Qazi Ahmad <107860183+QaziAhmad77@users.noreply.github.com>
2023-01-28 15:52:32 +0500

    component

--- src/components/weather/temp.js
+++ src/components/weather/temp.js
@@ -3,15 +3,16 @@ import Weathercard from "./weathercard";
 import "./style.css";
 
 const Temp = () => {
-  const [searchValue, setSearchValue] = useState("pune");
+  const [searchValue, setSearchValue] = useState("Peshawar");
   const [tempInfo, setTempInfo] = useState({});
 
   const getWeatherInfo = async () => {
     try {
-      let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid={WriteYourAPIKey}`;
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&appid=3265874a2c77ae4a04bb96236a642d2f&units=metric`;
 
       let res = await fetch(url);
       let data = await res.json();
+      console.log(data)
 
       const { temp, humidity, pressure } = data.main;
       const { main: weathermood } = data.weather[0];
@@ -38,26 +39,16 @@ const Temp = () => {
 
   useEffect(() => {
     getWeatherInfo();
+    // eslint-disable-next-line
   }, []);
 
   return (
     <>
       <div className="wrap">
         <div className="search">
-          <input
-            type="search"
-            placeholder="search..."
-            autoFocus
-            id="search"
-            className="searchTerm"
-            value={searchValue}
-            onChange={(e) => setSearchValue(e.target.value)}
-          />
+          <input type="search" placeholder="search..." autoFocus id="search" className="searchTerm" value={searchValue} onChange={(e) => setSearchValue(e.target.value)} />
 
-          <button
-            className="searchButton"
-            type="button"
-            onClick={getWeatherInfo}>
+          <button className="searchButton" type="button" onClick={getWeatherInfo}>
             Search
           </button>
         </div>
